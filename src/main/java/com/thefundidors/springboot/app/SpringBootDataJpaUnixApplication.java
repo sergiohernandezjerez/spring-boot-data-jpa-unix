3666eadd8eb305a52dd8b825e7a4eb60d9d9acf6
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-10-01 18:10:41 +0200

    Start JPA application

---
+++ src/main/java/com/thefundidors/springboot/app/SpringBootDataJpaUnixApplication.java
@@ -0,0 +1,13 @@
+package com.thefundidors.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaUnixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaUnixApplication.class, args);
+	}
+
+}
